b88af4114e5a8dfd95b2d7b34bd5897c518cdfa0
John Mertic <jmertic@linuxfoundation.org>
2024-08-17 09:21:00 -0400

    keep trying
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- lfx_landscape_tools/lfxmembers.py
+++ lfx_landscape_tools/lfxmembers.py
@@ -31,6 +31,8 @@ class LFXMembers(Members):
             memberList = endpointResponse.json()
             for record in memberList:
                 record['Website'] = '' if 'Website' not in record else record['Website']
+                logger.info(record)
+                logger.info("Test account" in record['Name'])
                 if self.find(record['Name'],record['Website'],record['Membership']['Name']) or "Test account" in record['Name']:
                     continue
 
