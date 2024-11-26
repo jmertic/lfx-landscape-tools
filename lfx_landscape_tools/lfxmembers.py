d53c913c2f5d20c55aa074f2b38bb73071a77257
John Mertic <jmertic@linuxfoundation.org>
2024-11-26 16:28:23 -0500

    Add option for adding other project memberships under the "Project Memberships" category
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- lfx_landscape_tools/lfxmembers.py
+++ lfx_landscape_tools/lfxmembers.py
@@ -9,6 +9,7 @@ import logging
 
 # third party modules
 import requests
+import requests_cache
 
 from lfx_landscape_tools.members import Members
 from lfx_landscape_tools.member import Member
@@ -20,10 +21,12 @@ class LFXMembers(Members):
     project = ''
     endpointURL = 'https://api-gw.platform.linuxfoundation.org/project-service/v1/public/projects/{}/members?orderBy=name&status=Active,At Risk' 
     endpointURLUsePublicMembershipLogo = 'https://api-gw.platform.linuxfoundation.org/project-service/v1/public/projects/{}/members?orderBy=name&status=Active,At Risk&usePublicMembershipLogo=true' 
-    
+    endpointURLAllAutoJoinProjects = 'https://api-gw.platform.linuxfoundation.org/project-service/v1/public/projects?autoJoinEnabled=true&pageSize=2000'
+
     def processConfig(self, config: type[Config]):
         self.project = config.project
         self.endpointURL = self.endpointURLUsePublicMembershipLogo if config.memberUsePublicMembershipLogo else self.endpointURL
+        self.addOtherProjectMemberships = config.addOtherProjectMemberships 
 
     def loadData(self):
         logger = logging.getLogger()
@@ -38,6 +41,7 @@ class LFXMembers(Members):
                 member = Member()
                 member.orgname = record.get('Name')
                 logger.info("Found LFX Member '{}'".format(member.orgname))
+                second_path = []
                 member.membership = record.get('Membership',{}).get('Name')
                 member.website = record.get('Website')
                 member.logo = record.get('Logo')
@@ -47,6 +51,15 @@ class LFXMembers(Members):
                 member.crunchbase = record.get('CrunchBaseURL')
                 member.twitter = record.get('Twitter')
                 member.linkedin = record.get('LinkedInURL')
+                if self.addOtherProjectMemberships:
+                    for slug in self.projectsOnAutojoin:
+                        session = requests_cache.CachedSession()
+                        with session.get(self.endpointURL.format(slug)) as otherProjectMembershipsEndpointResponse:
+                            for membership in otherProjectMembershipsEndpointResponse.json():
+                                if membership.get('ID') == record.get('ID'):
+                                    logger.info("Adding other membership - {}".format(membership.get("ProjectName")))
+                                    second_path.append('Project Membership / {}'.format(membership.get("ProjectName")))
+                member.second_path = second_path
                 self.members.append(member)
 
     def find(self, org, website, membership = None):
@@ -59,6 +72,18 @@ class LFXMembers(Members):
                 members.append(member)
 
         return members
+
+    @property
+    def projectsOnAutojoin(self):
+        session = requests_cache.CachedSession()
+        slugs = []
+        with session.get(self.endpointURLAllAutoJoinProjects) as endpointResponse:
+            projects = endpointResponse.json()
+            for project in projects['Data']:
+                if self.project != project.get('Slug'):
+                    slugs.append(project.get('Slug'))
+
+        return slugs
 
     def _isTestRecord(self,record):
         return record.get('Name') == "Test account" or record.get('ID') == '0012M00002WQimKQAT'
